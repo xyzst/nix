a5660c5df880a5f83b05f0918fe8e9da8192f8d7
Darren Rambaud <darren@rambaud.io>
2024-04-21 15:52:30 -0500

    std: replace timesyncd with ntp
    Related to: #4

--- std/nixos/configuration.nix
+++ std/nixos/configuration.nix
@@ -162,6 +162,9 @@
   };
 
   services = {
+    ntp = {
+      enable = true; # by default, nixos uses `systemd-timesyncd`
+    };
     openssh = {
       banner = ''🤠 Howdy, partner!
       << If you're reading this, you've been in a coma for almost 20 years now. We're trying a new technique. We don't know where this message will end up in your dream, but we hope it works. Please wake up, we miss you. >>
